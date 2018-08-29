55ace7fe580f52dea530bcbf03ec41e5ba58a239
bolshaksm <bmsergius@gmail.com>
2018-08-29 22:50:04 +0300

    3 step
    Рандомизировал движение рыб.

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ window.onload = function() {
      movement ();
 }
 
-let fish = document.getElementById('fish'),
-	predator = document.getElementById('predator');
+const  fish = document.getElementById('fish'),
+	   predator = document.getElementById('predator');
 	
 function movement() {
+
 	setInterval (
     	function () {
-    	predator.style.transform += 'translateX(1px)';
-    }, 10);
+                let moveUp = 'translateY(-' + (Math.random() * 20) + 'px)',
+                    moveRight = 'translateX(' + (Math.random() * 20) + 'px)',
+                    moveDown = 'translateY(' + (Math.random() * 20) + 'px)',
+                    moveLeft = 'translateX(-' + (Math.random() * 20) + 'px)',
+                    arr = [moveUp, moveRight, moveDown, moveLeft];
+
+    				fish.style.transition = 'all 0.3s linear';
+    				predator.style.transition = 'all 0.3s linear';
+    					
+    				fish.style.transform += arr[Math.floor(Math.random() * arr.length)];
+    				predator.style.transform += arr[Math.floor(Math.random() * arr.length)];
+    				}, 
+    				300
+    			);
 }
